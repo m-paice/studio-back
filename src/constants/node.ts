5e4ec1bb3a3bf76abe281918cc3cc81e2dc8e8ee
Matheus Paice <matheus.paice@jbtec.com.br>
2023-08-09 09:33:54 -0300

    fix: corrige leitura das variaveis de ambiente

--- src/constants/node.ts
+++ src/constants/node.ts
@@ -1,4 +1,3 @@
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 
-export const VERIFY_TOKEN = process.env.VERIFY_TOKEN
-  || 'BD13A5BFF9D92339BCD49910CAE3418D2D326F18DC3731E52A8153B5441C97C6';
+export const VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'hash_fake';
