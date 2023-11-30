dcddcd43ccaeae241eb73a6a78a9d3e02040159e
Matheus Paice <matheus.paice@jbtec.com.br>
2023-11-30 09:26:29 -0300

    fix: corrige payload

--- src/microservice/whatsapp/send.ts
+++ src/microservice/whatsapp/send.ts
@@ -12,7 +12,7 @@ export async function sendMessage({ campaign, schedule }: Send) {
   const template = await resource.Templates.findById(campaign.templateId);
 
   const payload = {
-    message: template.content,
+    content: template.content,
     phoneNumber: `55${schedule.user.cellPhone}`,
     info: {
       campaignId: campaign.id,
