20c27e47c194920c07265f66256bb12f5e1c49e3
Matheus Paice <matheus.paice@jbtec.com.br>
2023-12-10 07:37:23 -0300

    fix: remove account expired

--- src/microservice/cron/index.ts
+++ src/microservice/cron/index.ts
@@ -1,13 +1,12 @@
 import Schedule from 'node-schedule';
 
 import { handleCampaigns } from './campaigns';
-import { handleAccountExpired } from './accountExpired';
 
 type Job = () => void;
 
 const cronJobs: [string, Job][] = [
   ['* * * * *', handleCampaigns], // every minute
-  ['0 0 * * *', handleAccountExpired], // every day at 00:00
+  // ['0 0 * * *', handleAccountExpired], // every day at 00:00
 ];
 
 export const cronStart = () => {
